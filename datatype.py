4b68737f429bf28cdb93eb7564d099e682767eff
Bibha kumari <bibhansh2009@gmail.com>
2026-06-11 09:04:40 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,22 @@
+
+name="anshu"
+age=24
+hight=154
+my_Name_is_anshu=True
+
+ print(type(my_Name_is_anshu))
+
+def add(a,b):
+      return(a*b)     
+result=add(9,6)
+ print(result)
+# case sensitive
+name="anshu"
+Name="anshu"
+NAME="anshu"
+print(name,NAME,Name)
+ print(type(NAME))
+ print(type(Name))
+ print(type(name))
+
+    
