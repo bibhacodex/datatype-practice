3d3b0ebe861d36ba572dbe5fb3821cd141b5c7f9
Bibha kumari <bibhansh2009@gmail.com>
2026-06-11 09:15:26 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -4,19 +4,19 @@ age=24
 hight=154
 my_Name_is_anshu=True
 
- print(type(my_Name_is_anshu))
+print(type(my_Name_is_anshu))
 
 def add(a,b):
       return(a*b)     
 result=add(9,6)
- print(result)
+print(result)
 # case sensitive
 name="anshu"
-Name="anshu"
-NAME="anshu"
+Name="bibha"
+NAME="kumari"
 print(name,NAME,Name)
- print(type(NAME))
- print(type(Name))
- print(type(name))
+print(type(NAME))
+print(type(Name))
+print(type(name))
 
     
